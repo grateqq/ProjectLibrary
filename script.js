64472b39669c467188018705245259745dc1e83e
grateqq <grateqq@users.noreply.github.com>
2026-03-26 00:44:33 -0300

    funcionando tabla con botones. falta agregar funcion de carga

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("hola")
 
 const libreria = [];
 
+
 //F. constructura
 function Book (author, pages, name ) {
   this.author = author;
@@ -22,6 +23,82 @@ function readLibrary (libreria) {
  })
 }
 
+//F. Clear tabla
+
+function resettable() {
+  const table = document.getElementById("table-body")
+  table.innerHTML = ``
+}
+// F. Elimina libro segun id
+function deleteindice(indice) {
+  libreria.splice(indice,1)
+  console.log("corte-----------")
+  readLibrary(libreria)
+  printlibrary(libreria)
+}
+//F. Buscar indice segun id
+function indiceID (idlibro) {
+
+  const indice = libreria.findIndex(libro => libro.id === idlibro);
+    console.log(indice)
+}
+
+
+//F visualiza en la tabla todos los libros
+
+function printlibrary () {
+
+   libreria.forEach(function(libro) {
+    const tbody= document.getElementById ("table-body");
+    // console.log(tbody); 
+
+    // console.log(libro.name); console.log(libro.pages);console.log(libro.author)
+     const newtr = document.createElement('tr')
+      newtr.innerHTML = `
+
+        <th>${libro.name}</th>
+        <th>${libro.author}</th>
+        <th>${libro.pages}</th>
+        <th><button class="btn-delete" data-id="${libro.id}" >X</button></th>   
+        
+      `
+    tbody.appendChild(newtr)
+  })
+  const btndelete = document.querySelectorAll(".btn-delete")
+  console.log(btndelete)
+  btndelete.forEach(function(button){
+    button.addEventListener("click", function(event) {
+      const idbook = button.dataset.id
+      console.log(button.dataset.id)
+      const indiceBook = indiceID(idbook)
+      console.log(indiceBook)
+      deleteindice(indiceBook)
+      console.log(libreria)
+      resettable()
+      printlibrary()
+    })
+  })
+  
+  
+}
+//Ej
+
+
+
+// Intereccion de usuario con funciones
+
+const btnopenform = document.getElementById("btn-openform")
+// console.log(btnopenform)
+
+const formnewbook = document.getElementById("formaddbook")
+// console.log(formnewbook)
+
+btnopenform.addEventListener('click', function() {
+  formnewbook.style.display = 'block';  // o 'flex', 'grid' según necesites
+});
+
+
+console.log("ok----")
 //Ejemplo Carga inicial
 addbooktolibrary ("jere", 200, "Hilo Rojo")
 addbooktolibrary ("mica", 300, "Hilo Azul")
@@ -31,43 +108,8 @@ addbooktolibrary ("miche", 400, "TapaDorada")
 
 //test 
 readLibrary(libreria)
-console.log("ok----")
-
-//F visualiza en la tabla todos los libros
-
-function printlibrary (libreria) {
- 
-  libreria.forEach(function(libro) {
-    const tbody= document.getElementById ("table-body");
-    console.log(tbody); 
-
-    console.log(libro.name); console.log(libro.pages);console.log(libro.author)
-     const newtr = document.createElement('tr')
-      newtr.innerHTML = `
-
-        <th>${libro.name}</th>
-        <th>${libro.author}</th>
-        <th>${libro.pages}</th>
-      </tr>
-      `
-      tbody.appendChild(newtr)
-    
- })
-}
-//Ej
 
 printlibrary(libreria)
-console.clear()
+// console.clear()
 
-// Intereccion de usuario con funciones
-
-const btnopenform = document.getElementById("btn-openform")
-console.log(btnopenform)
-
-const formnewbook = document.getElementById("formaddbook")
-console.log(formnewbook)
-
-btnopenform.addEventListener('click', function() {
-  formnewbook.style.display = 'block';  // o 'flex', 'grid' según necesites
-});
-
+// resettable()
